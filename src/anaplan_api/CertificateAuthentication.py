65198f5ee1fd3e2aeabf0e456ab97f889bf145a5
jeswils-ap <jeswils@gmail.com>
2021-11-03 15:32:27 +0800

    Update class to better check for key string vs. file path. Add support to read key from file.

--- src/anaplan_api/CertificateAuthentication.py
+++ src/anaplan_api/CertificateAuthentication.py
@@ -8,13 +8,14 @@
 # Input:			Public certificate file location, private key file location
 # Output:		Authorization header string, request body string containing a nonce and its signed value
 #==============================================================================
-import os, logging
+
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 from base64 import b64encode
 from typing import List
-from anaplan_api.AnaplanAuthentication import AnaplanAuthentication
+from AnaplanAuthentication import AnaplanAuthentication
+import os, logging
 
 logger = logging.getLogger(__name__)
 
@@ -23,18 +24,17 @@ class CertificateAuthentication(AnaplanAuthentication):
 	# This function reads a user's public certificate as a string, base64 
 	# encodes that value, then returns the certificate authorization header.
 	#===========================================================================
-	def auth_header(self, pubCert) -> str:
+	def auth_header(self, pub_cert: str) -> str:
 		'''
 		:param pubCert: Path to public certificate
 		'''
 
-		if(pubCert[:5] == "-----"):
-			my_pem_text=pubCert
+		if not os.path.isfile(pub_cert):
+			my_pem_text = pub_cert
 		else:
-			with open(pubCert, "r") as my_pem_file:
+			with open(pub_cert, "r") as my_pem_file:
 				my_pem_text = my_pem_file.read()
 
-		#header_string = { 'AUTHORIZATION' : 'CACertificate ' + b64encode(my_pem_text.encode('utf-8')).decode('utf-8') }
 		header_string = { 'AUTHORIZATION' : ''.join(['CACertificate ', b64encode(my_pem_text.encode('utf-8')).decode('utf-8')])}
 
 		return header_string
@@ -44,13 +44,13 @@ class CertificateAuthentication(AnaplanAuthentication):
 	# then the function to sign the nonce, and finally returns the Anaplan authentication
 	# POST body value
 	#===========================================================================	
-	def generate_post_data(self, privKey) -> str:
+	def generate_post_data(self, priv_key: str) -> str:
 		'''
-		:param privKey: Path to private key
+		:param priv_key: Private key text or path to key
 		'''
 
 		unsigned_nonce = CertificateAuthentication.create_nonce()
-		signed_nonce=str(CertificateAuthentication.sign_string(unsigned_nonce, privKey))
+		signed_nonce = str(CertificateAuthentication.sign_string(unsigned_nonce, priv_key))
 
 		#json_string='{ "encodedData":"' + str(b64encode(unsigned_nonce).decode('utf-8')) + '", "encodedSignedData":"' + signed_nonce + '"}'
 		json_string = ''.join(['{ "encodedData":"', str(b64encode(unsigned_nonce).decode('utf-8')), '", "encodedSignedData":"', signed_nonce, '"}'])
@@ -72,16 +72,20 @@ class CertificateAuthentication(AnaplanAuthentication):
 	# with the private key.
 	#===========================================================================
 	@staticmethod
-	def sign_string(message, privKey) -> str:
+	def sign_string(message: str, priv_key: str) -> str:
 		'''
 		:param message: 150-character psuedo-random byte-array of characters
-		:param privKey: Private key text, used to since the message.
+		:param priv_key: Private key text, used to sign the message.
 		:returns: B64 encoded signed nonce
 		'''
 
 		backend = default_backend()
 		try:
-			key = serialization.load_pem_private_key(privKey, None, backend=backend)
+			if not os.path.isfile(priv_key):
+				key = serialization.load_pem_private_key(priv_key, None, backend=backend)
+			else:
+				with open(priv_key, 'r') as key_file:
+					key = serialization.load_pem_private_key(key_file.read().encode('utf-8'), None, backend=backend)
 		except ValueError as e:
 			logger.error(f"Error loading private key {e}")
 		try:	
