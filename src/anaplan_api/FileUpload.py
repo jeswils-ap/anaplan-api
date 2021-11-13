2c6b96bb254e8cee6702ec1102f938db85c7cc0f
jeswils-ap <jeswils@gmail.com>
2021-11-13 12:55:59 +0800

    Update Upload sub-classes to improve file/string read mechanism.

--- src/anaplan_api/FileUpload.py
+++ src/anaplan_api/FileUpload.py
@@ -1,4 +1,5 @@
 import logging
+from functools import partial
 from .Upload import Upload
 
 logger = logging.getLogger(__name__)
@@ -20,15 +21,10 @@ class FileUpload(Upload):
 			logger.info("Starting file upload.")
 
 			with open(file, 'rt') as file:
-				file_data = []
 				chunk_num = 0
-				while True:
-					buf = file.readlines((1024 * 1024) * chunk_size)
-					if not buf:
-						break
-					for item in buf:
-						file_data.append(item)
-					complete = super().file_data(url, chunk_num, ''.join(file_data).encode('utf-8'))
+
+				for block in iter(partial(file.read, chunk_size * 1024**2), ''):
+					complete = super().file_data(url, chunk_num, block.encode('utf-8'))
 					chunk_num += 1
 
 			if complete:
