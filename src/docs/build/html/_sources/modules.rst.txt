45d675bd8caff62f84cfd22e963433890d628c01
Jesse Wilson <jeswils@gmail.com>
2024-03-11 14:58:14 +0800

    Refactor package structure

--- src/docs/build/html/_sources/modules.rst.txt
+++
@@ -1,7 +0,0 @@
-anaplan_api
-===========
-
-.. toctree::
-   :maxdepth: 4
-
-   anaplan_api
---
+++ src/models/UserDetails.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from src.util.strtobool import strtobool
+
+
+@dataclass
+class UserDetails(object):
+    _user_details: dict
+    _email: str
+    _id: str
+    _active: bool
+    _email_opt_in: bool
+    _last_login: datetime
+
+    def __init__(self, details: dict):
+        """
+        :param details: JSON details for specified user
+        :type details: dict
+        """
+        self._user_details = details
+        self._id = details["id"]
+        self._email = details["email"]
+        self._active = strtobool(str(details["active"]))
+        self._email_opt_in = strtobool(str(details["emailOptIn"]))
+        self._last_login = datetime.strptime(
+            str(details["lastLoginDate"]), "%Y-%m-%dT%H:%M:%S.%f%z"
+        )
+
+    def __str__(self) -> str:
+        """Return details for specified user
+
+        :return: User details
+        :rtype: str
+        """
+        return f"Email: {self._email}, ID: {self._id}, Active: {self._active}, Last login: {self._last_login}"
